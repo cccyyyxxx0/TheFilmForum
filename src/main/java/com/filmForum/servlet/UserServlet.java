faeaf1ed6c4ffd1241e6044c5f63c69d77649af8
wz0518 <954946296@qq.com>
2020-12-09 15:09:14 +0800

    更新userservlet signup

--- src/main/java/com/filmForum/servlet/UserServlet.java
+++ src/main/java/com/filmForum/servlet/UserServlet.java
@@ -3,12 +3,22 @@ package com.filmForum.servlet;
 import com.filmForum.entity.User;
 import com.filmForum.service.UserService;
 import com.filmForum.service.impl.UserServiceImpl;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.List;
+
 import static com.google.code.kaptcha.Constants.KAPTCHA_SESSION_KEY;
 
 /**
@@ -27,6 +37,21 @@ public class UserServlet extends BaseServlet{
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         super.doGet(request, response);
     }
+
+    public void signup(HttpServletRequest request,HttpServletResponse response) throws UnsupportedEncodingException {
+        //防止中文乱码
+        response.setContentType("text/html;charset=utf-8");
+
+        String username =request.getParameter("username");
+        String password=request.getParameter("password");
+        String repassword=request.getParameter("repassword");
+        String email=request.getParameter("email");
+        String phone=request.getParameter("phone");
+        if (password.equals(repassword)){
+            int result=userService.insert(new User(username,password,phone,email));
+        }
+    }
+
 
     public void login(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String username =request.getParameter("username");
@@ -50,4 +75,5 @@ public class UserServlet extends BaseServlet{
 //        }
 
     }
+
 }
